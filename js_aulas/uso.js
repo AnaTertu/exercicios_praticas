8ce3bb7da16da4fa7792b2edf1e3cc97c6d0265c
Ana Carolina Tertuliano Santos <ana.tertu.2019@gmail.com>
2021-11-08 15:15:20 -0300

    exescicio treinamento

---
+++ js_aulas/uso.js
@@ -0,0 +1,65 @@
+colaboradores    = ['Rhoger', 'Cléverson', 'Gleysberto', 'Nádio', 'Rosiméri', 'Regislane', 'Agripino', 'Wellison']
+idades           = [      69,          66,           58,      61,         60,          64,         59,         55]
+temposDeTrabalho = [      31,          29,           30,      26,         25,          24,         26,         29]
+
+/* Função para verificar se um colaborador está apto para se aposentar, atendendo a pelo menos uma das seguintes regras:
+ * - Ter pelo menos 65 anos de idade;
+ * - Ter trabalhado no mínimo 30 anos;
+ * - Ter pelo menos 60 anos de idade e 25 anos de trabalho.
+ */
+function verificarAposentadoria(nome) {
+
+    colaborador = false
+    idade = 0
+    tempoTrabalho = 0
+
+    // buscando um colaborador pelo nome
+    for (posicao = 0; posicao < colaboradores.length; posicao++) {
+
+        if (nome == colaboradores[posicao]) {
+            colaborador = true
+            idade = idades[posicao]
+            tempoTrabalho = temposDeTrabalho[posicao]
+        }
+    }
+
+    // se encontrar o colaborador, verifica as regras de aposentadoria
+    if (colaborador) {
+        
+        porIdade = (idade >= 65)
+        porTempoDeTrabalho = (tempoTrabalho >= 30)
+        porIdadeComTempoDeTrabalho = (idade >= 60 && tempoTrabalho >= 25)
+
+        // não importa qual regra seja atendida
+        // se qualquer uma for, o colaborador pode se aposentar
+        if (porIdade || porTempoDeTrabalho || porIdadeComTempoDeTrabalho) {
+            console.log("Parabéns, já pode sair de férias eternas :)")
+        } else {
+            console.log("Infelizmente, ainda falta um tempo :(")
+        }
+    
+    } else {
+        console.log("Colaborador inválido :|")
+    }
+}
+
+verificarAposentadoria('Rosiméri')    // true (regra 3)
+verificarAposentadoria('Agripino')    // false (falta 1 ano de idade para regra 3)
+verificarAposentadoria('Rhoger')      // true (todas)
+verificarAposentadoria('Hudnélson')   // false (colaborador inexistente)
+verificarAposentadoria('Gleysberto')  // true (regra 2)
+verificarAposentadoria('Regislane')   // false (falta 1 ano de idade para regra 1 e 1 de trabalho para regra 3)
+verificarAposentadoria('Cléverson')   // true (regras 1 e 3)
+verificarAposentadoria('Nádio')       // true (regra 3)
+verificarAposentadoria('Wellison')    // false (falta 1 ano de trabalho para regra 2)
+verificarAposentadoria('Florisberto') // false (colaborador inexistente)
+
+// numero = 1
+
+// while (numero <= 10){
+//     console.log(numero)
+//     numero += 0
+// }
+
+// if (numero > 10)
+// console.log(numero)
